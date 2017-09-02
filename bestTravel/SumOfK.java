7926c893ccf7b49cfc6b7a814bd208b69f747026
decimater <richard.millett@googlemail.com>
2017-09-02 15:49:07 +0100

    best travel, trying new coding style

---
+++ bestTravel/SumOfK.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.util.stream.IntStream;
+import static java.util.stream.Collectors.toSet;
+
+public class SumOfK {
+    public static void main(String[] args) {
+	System.out.println(chooseBestSum(163, 3, new ArrayList<>(Arrays.asList(50, 55, 56, 57, 58))));
+	System.out.println(chooseBestSum(163, 3, new ArrayList<>(Arrays.asList(50))));
+	System.out.println(chooseBestSum(230, 3, new ArrayList<>(Arrays.asList(91, 74, 73, 85, 73, 81, 87))));
+    }
+    
+    public static Integer chooseBestSum(int maxDistance, int totalPlacesToVisit, List<Integer> ls) {
+        List<Integer> distances = new ArrayList<>(ls);
+        distances.removeIf(i -> i == null || i < 0);
+        
+        //Get all combinations
+        Set<String> distinctBinaryCombinations = IntStream.range(1, 1 << distances.size())
+                .filter(i -> Integer.bitCount(i) == totalPlacesToVisit)
+                .mapToObj(i -> String.format("%" + distances.size() + "s", Integer.toBinaryString(i)).replace(' ', '0'))
+                .collect(toSet());
+                
+        // add up all the groups
+        List<Integer> totals = new LinkedList<>();
+
+        for (String bin : distinctBinaryCombinations) {
+            int total = 0;
+            for(int i = 0; i < bin.length(); i++) {
+                if (bin.charAt(i) == '1') {
+                    total+= distances.get(i);
+                }
+            }
+
+            totals.add(total);
+        }
+        
+        // return closest if not possible null
+        return totals.stream().filter(i -> i <= maxDistance)
+                .sorted( Comparator.comparingInt(i -> maxDistance - i))
+                .findFirst().orElse(null);
+    }
+}
