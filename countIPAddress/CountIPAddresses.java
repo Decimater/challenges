5c4bf6c1fc13d53ea44227970f2e03a103782909
decimater <richard.millett@googlemail.com>
2017-10-01 16:14:32 +0100

    ip range calc

---
+++ countIPAddress/CountIPAddresses.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class CountIPAddresses {
+	public static void main(String[] args) {
+		System.out.println(ipsBetween("10.0.0.0", "10.0.0.50")); // 50
+		System.out.println(ipsBetween("20.0.0.10", "20.0.1.0")); // 246
+	}
+
+	/*
+	*	IP v4 addresses are 32 numbers 4 bytes separated by '.' and are unsigned
+	*/
+	public static long ipsBetween(String start, String end) {
+		System.out.println("s: " + start + "\te:" + end);
+		
+		String startIP = Arrays.stream(start.split("\\."))
+					.map(Integer::parseInt)
+					.map(i -> String.format("%8s", Integer.toBinaryString(i)).replace(' ', '0') )
+					.collect(Collectors.joining());
+				  
+		String endIP = Arrays.stream(end.split("\\."))
+					.map(Integer::parseInt)
+					.map(i -> String.format("%8s", Integer.toBinaryString(i)).replace(' ', '0') )
+					.collect(Collectors.joining());
+					
+		return Long.parseLong(endIP, 2) - Long.parseLong(startIP, 2);
+	}
+}
