45102c24225a6b5e42f534adfb7277a7ee2e55c4
decimater <richard.millett@googlemail.com>
2018-03-24 19:37:12 +0100

    9 to 5

---
+++ countWorkDays/CountWorkingDays.java
@@ -0,0 +1,44 @@
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import static java.time.DayOfWeek.FRIDAY;
+import static java.time.DayOfWeek.MONDAY;
+import static java.time.temporal.ChronoUnit.WEEKS;
+import static java.time.temporal.TemporalAdjusters.lastDayOfMonth;
+import static java.time.temporal.TemporalAdjusters.nextOrSame;
+import static java.time.temporal.TemporalAdjusters.previousOrSame;
+
+public class CountWorkingDays {
+
+    public static void main(String[] args) {
+        System.out.println(count(LocalDate.of(2017, 6, 1), LocalDate.of(2017, 6, 1))); // 20
+		System.out.println(countBetter(LocalDate.of(2017, 6, 1), LocalDate.of(2017, 6, 1))); // 20
+    }
+	
+	static long count(final LocalDate start, final LocalDate end) {
+		LocalDate newStart = LocalDate.of(start.getYear(), start.getMonth(), 1);
+		boolean isMonday = newStart.getDayOfWeek().equals(DayOfWeek.MONDAY);
+		if (!isMonday) {
+			int weekDayValue = start.getDayOfWeek().getValue();
+			int diff = Math.abs(7 - weekDayValue) + 1;
+			newStart = newStart.plusDays(diff);
+		}
+      
+		int daysInMonth = end.lengthOfMonth();
+        LocalDate newEnd = LocalDate.of(end.getYear(), end.getMonth(), daysInMonth);
+		
+        while (!newEnd.getDayOfWeek().equals(DayOfWeek.FRIDAY)) {
+			newEnd = newEnd.minusDays(1);
+        }
+		
+		return (WEEKS.between(newStart, newEnd) + 1) * 5;
+    }
+
+	static long countBetter(final LocalDate start, final LocalDate end) {
+		LocalDate startMonday = start.with(nextOrSame(DayOfWeek.MONDAY));
+        LocalDate endFriday = end
+                .with(lastDayOfMonth())
+                .with(previousOrSame(DayOfWeek.FRIDAY));
+				
+		return (WEEKS.between(startMonday, endFriday) +1) * 5;
+	}
+}
---
+++ countWorkDays/readme.txt
@@ -0,0 +1,15 @@
+https://www.codewars.com/kata/how-many-working-days/java
+
+How many working days
+
+You work in Operations at Great Projects Ltd. Helen, from Sales, is about to bid for a contract. The contract specifies the month when the project starts and the month when the project ends. She wants you to calculate how many working days there are in the duration of this project.
+
+    Working days in a week are Monday to Friday.
+    Non working days are Saturday and Sunday. There are no holidays.
+    You will get a start date and an end date. Disregard the day in these dates. Only note the month and the year.
+    The project will start on the first Monday of the month in the start date.
+    It will be completed on the last Friday of the month in the end date.
+    If the end date is the same as the start date then this is a 1 month project.
+    In the tests, both start and end will be valid non-null dates and the end date will be the same as or later than the start date.
+
+Create a method to calculate the number of working days in this project
